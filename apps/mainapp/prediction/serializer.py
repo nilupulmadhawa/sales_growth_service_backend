ca292a225e6c2477deca7a8f028aa2f20fddba80
SandalikaAriyrathna <dilsandu96@gmail.com>
2024-03-16 18:40:20 +0530

    remove

--- apps/mainapp/prediction/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from . models import *
-
-
-class MultiCategoryDatasetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MultiCategoryDataset
-        fields = ['event_time', 'event_type', 'product_id', 'category_id', 'category_code', 'brand', 'price', 'user_id', 'user_session', 'age','gender', 'location']
-    
--- apps/mainapp/prediction/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('get_user_recommendations/', views.get_user_recommendations, name='get_user_recommendations'),
-]
-
-
